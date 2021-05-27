21c5591f80098e85d1aee5ac272d682739f4d0f7
Fadi Mashan <fadi.mashan@gmail.com>
2021-05-27 13:04:53 +0200

    Add Project Api

---
+++ MachineB2C/Entities/MachineResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MachineB2C.Entities
+{
+    public class MachineResponse
+    {
+        public bool Success { get; set; }
+
+        public List<string> ErrorMessages { get; set; }
+
+        public List<MachineInfo> Machine { get; set; }
+    }
+}
